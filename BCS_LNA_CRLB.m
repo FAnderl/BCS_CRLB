0043720a07b8425b836545dd04545361fdf5cb94
Florian Anderl <24881307+FAnderl@users.noreply.github.com>
2024-06-04 17:22:59 +0200

    Some Changes + Added Toy Example w/ easier toy model structure

--- BCS_LNA_CRLB.m
+++ BCS_LNA_CRLB.m
@@ -215,7 +215,7 @@ delta_RR = 0.01;
 delta_TF = 0.1;
 delta_P = 0.1;
 
-kappa_up = 0.001;
+kappa_up = 0.3;
 K_up = 0.1;
 kappa_RR = 0.1;
 k_TF_m = 0.1;
@@ -275,11 +275,16 @@ RR = init_states(3)
 TF = init_states(4)
 P = init_states(5)
 
+% Initia time for J0 - > J1
+t = 0
+
 % J1
 subs(D_11)
 subs(D_21)
 subs(D_12)
 subs(D_22)
+
+
 
 J = eval(D_22) - (eval(D_21)*pinv(J_0 + eval(D_11))) * eval(D_12);
 
@@ -305,13 +310,18 @@ for i = 2:num_t_steps
     % Guard against *DIVISION BY ZERO*
     if S_ext == 0
         S_ext = 0.0001;
-    elseif S_int == 0
+    end
+    if S_int == 0
         S_int = 0.0001;
-    elseif RR == 0
-        RR = 0.0001;    
-    elseif TF == 0
+    end
+
+    if RR == 0
+        RR = 0.0001;  
+    end
+    if TF == 0
         TF = 0.0001;
-    elseif P == 0
+    end
+    if P == 0
         P = 0.0001;
     end
     
@@ -352,4 +362,191 @@ ylabel("Inverse FIM Element (1,1)")
 
 
 
+
+
+%% Easy facilitated toy example with fewer states and simple propensity function vector
+
+clear
+% Define State Variables 
+syms X1 X2 real
+% Vector x of State Variables
+x_vec = [X1; X2];
+assumeAlso(x_vec,'real')
+
+% Parameters 
+syms k1 k2 d1 d2 real
+params = [k1, k2, d1, d2];
+
+% Stoichiometry Matrix
+syms S real
+S = sym([
+    1 -1 -1 0 ;
+    0 0  1 -1 
+]);
+
+% Define Propensity Functions 
+syms a1(t, X1, X2) a2(t, X1, X2) real
+a1(t, X1) = k1;
+deg1(t, X1) = d1 * X1; 
+a2(t, X1) = k2 * X1;
+deg2(t, X2) = d2 * X2; 
+
+% Placeholder Argument with the right dimensions
+syms u [2 1] real;
+
+% Vector a of Propensity Functions
+a_vec(u) = [a1(t, u(1));deg1(t, u(1)) ; a2(t, u(1)); deg2(t, u(2))];
+
+
+% Define State Space System
+F(u) = u + S * a_vec(u(1), u(2));
+
+% Mean 
+z_np1 = F(x_vec(1), x_vec(2));
+
+% Covariance Matrix
+G(u) = diag(S * sqrt(a_vec(u(1), u(2))));
+v_np1 = G(x_vec(1), x_vec(2));
+Sigma_n = v_np1 * v_np1.';
+
+% D^11
+D_11 = sym(zeros(2, 2), 'r');
+for i = 1:2
+    for j = 1:2
+        D_11(i,j) = diff(z_np1.', x_vec(i)) * pinv(Sigma_n) * diff(z_np1, x_vec(j))  + ...
+            1/2 * trace(pinv(Sigma_n) * diff(Sigma_n,x_vec(i)) * pinv(Sigma_n)* diff(Sigma_n,x_vec(j)));
+    end
+end
+
+% D^12
+syms D_12  real % Matrix (2x2)
+D_12 = - jacobian(z_np1.',x_vec) * pinv(Sigma_n) ;
+
+% D^21
+D_21 = D_12.';
+
+% Observation Model (assumed Gaussian)
+syms xi sigma_obs real
+
+% D^22
+h = [0; xi*X2];    % Observation vector
+D_22 = inv(Sigma_n) +  (jacobian(h.', x_vec)  *   1/(sigma_obs^2)  * jacobian(h.', x_vec).') ;
+
+% Setting up ODE state space model for solving
+syms X1(t) X2(t)   % state variables
+rhs = [diff(X1(t), t); diff(X2(t), t)] == S*a_vec(x_vec(1), x_vec(2));
+
+% Defining Symbolic ODE System
+diff_X1 = rhs(1);
+diff_X2 = rhs(2);
+eq_sys = [diff_X1; diff_X2];
+sys_vars =["X1", "X2"];
+vars = [X1(t), X2(t)];
+
+[MS,F] = massMatrixForm(eq_sys,vars);
+MS = odeFunction(MS,vars);
+F = odeFunction(F,vars,params);
+
+% Substitution with real, numerical values
+% Substitute Parameter Values
+k1 = 0.1;
+k2 = 0.2;
+d1 = 0.1; 
+d2 = 0.1; 
+
+% Observation Noise Variance
+sigma_obs = 1;
+xi = 1;
+
+% Define time step tau
+tau = 1;
+
+init_states_toy = [1, 1e-4];
+
+% Solve ODE system 
+F_ode=@(t,Y) F(t, Y, eval(subs(params)));
+t0 = 0;
+t_end = 10;
+y0 = init_states_toy;
+
+opt = odeset('RelTol',1e-6,'AbsTol',1e-6,'Vectorized','on',NonNegative=[1,2]);
+figure(10)
+sol = ode15s(F_ode,[t0:tau:t_end],y0,opt);
+ode15s(F_ode,[t0:tau:t_end],y0,opt);
+legend('X1', 'X2')
+title("Toy Example Dynamic Solution")
+
+
+sol_toy = deval(t0:tau:t_end, sol);
+
+% FIM RECURSION
+
+% Define the (initial ?) states
+
+J_0 = (zeros(2, 2));
+
+% Initial values for the states for computation of J_1; Values cannot be
+% zero due to *DIVISION BY ZERO*
+
+X1 = init_states_toy(1);
+X2 = init_states_toy(2);
+
+
+% Initia time for J0 - > J1
+t = 0;
+
+% J1
+subs(D_11);
+subs(D_21);
+subs(D_12);
+subs(D_22);
+
+J = eval(D_22) - (eval(D_21)*pinv(J_0 + eval(D_11))) * eval(D_12);
+
+% Container for FIM for each time step
+J_FIM = zeros(2, 2, 10);
+inv_J_FIM =zeros(2, 2, 10);
+
+num_t_steps = size(sol_toy,2);
+
+for i = 2:num_t_steps
+
+    % Update States - J_{n+1}
+    X1 = sol_toy(1, i);
+    X2 = sol_toy(2,i);
+
+    % Guard against *DIVISION BY ZERO*
+    if X1 == 0
+        X1 = 0.0001;
+    end
+    if X2 == 0
+        X2 = 0.0001;
+    end
+
+    % Set t (only relevant if gamma(t) is a function of t)
+    time_vec = t0:tau:t_end;
+    t = time_vec(i);
+
+    % Substitute - J_{n+1}
+    subs(D_11);
+    subs(D_21);
+    subs(D_12);
+    subs(D_22);
+
+    J = eval(D_22) - (eval(D_21)*pinv(J + eval(D_11))) * eval(D_12);
+
+    J_FIM(:,:,i) = J;
+    inv_J_FIM(:,:,i) = pinv(J);
+
+end
+
+
+
+% Plot the element of the inverse FIM corresponding to X1 (first element)
+figure(11)
+el = inv_J_FIM(2,2,:);
+plot(1:num_t_steps, squeeze(el))
+title("Inverse FIM Element (1,1) over Time")
+xlabel("Time")
+ylabel("Inverse FIM Element (1,1)")
 
